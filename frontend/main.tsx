87b5d4a13aaab977c649139c02ddc70ed1387c45
Aiden Tepper <aidenjtep@gmail.com>
2024-03-12 16:45:03 -0700

    add vercel analytics and speed

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
 import App from "./App.tsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <SpeedInsights />
+    <Analytics />
     <NextUIProvider>
       <div className="w-screen h-screen p-8 items-start justify-center box-border overflow-hidden">
         <App />
